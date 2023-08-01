5d0c5e6849aa0e9e7c12299f2883f95ba3e309a1
Zack Whitehouse <zackwhitehouse225@gmail.com>
2023-08-01 18:58:06 -0400

    added stuff

--- codeChallenges/codeChallenge2.js
+++ codeChallenges/codeChallenge2.js
@@ -1,4 +1,10 @@
 
+
+// Write a program that evaluates the status of the variables above, and uses logic to print one of three possible messages:
+// - If the driving record is good and user is over 25 years old, they should get a discount on the car rental
+// - If the user either has a good record or is over 25 years old, they should pay full price
+// - If the user is not over 25 and has a bad driving record, they need to have someone else sign for the rental
+
 var goodDrivingRecord = true;
 var age = 25;
 
